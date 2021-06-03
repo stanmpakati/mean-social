ebac5007cd5eacf6a8934e9c8bccaf7cbf8824a6
stan000 <stanmpakatig@gmail.com>
2021-06-03 16:31:02 +0200

    login routes and token generation done

--- backend/routes/controllers/users.js
+++ backend/routes/controllers/users.js
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import user from "../../models/user.js";
 import User from "../../models/user.js";
 
 export const getUsernames = (req, res) => {
@@ -8,7 +11,7 @@ export const getUsernames = (req, res) => {
 };
 
 export const getEmail = (req, res) => {
-  User.find({ email: req.body.email }).then((email) => {
+  User.find({ email: req.body.email.toLower() }).then((email) => {
     if (email.length !== 0) res.status(200).json({ message: "Found" });
     else res.json({ message: "Not found" });
   });
@@ -17,8 +20,8 @@ export const getEmail = (req, res) => {
 export const signup = (req, res) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
-      username: req.body.username,
-      email: req.body.email,
+      username: req.body.username.toLower(),
+      email: req.body.email.toLower(),
       password: hash,
     });
     user
@@ -35,4 +38,43 @@ export const signup = (req, res) => {
         });
       });
   });
+};
+
+export const login = (req, res) => {
+  user
+    .findOne({
+      $or: [
+        { username: req.body.username.toLower() },
+        { email: req.body.email.toLower() },
+      ],
+    })
+    .then((user) => {
+      // if user is not found
+      if (!user) return res.status(404).json({ message: "user not found" });
+
+      // if user found
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      // If passwords don't match
+      if (!result) return res.status(401).json({ message: "Invalid Password" });
+
+      // If passwords match
+      // Create jwt token
+      const token = jwt.sign(
+        {
+          email: user.email,
+          username: user.name,
+          userId: user._id,
+        },
+        "long_random_word_for_encryption_9qid&&50*,;(32",
+        { expiresIn: "1h" }
+      );
+
+      // Send response
+      res.status(200).json({ message: "logged in successefuly", token: token });
+    })
+    .catch((err) => {
+      console.log(err), res.status(500).json({ message: err });
+    });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsernames, getEmail, signup } from "./controllers/users.js";
+import { getUsernames, getEmail, login, signup } from "./controllers/users.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.get("/usernames", getUsernames);
 router.post("/email", getEmail);
 
 router.post("/signup", signup);
+
+router.post("/login", login);
 
 export default router;
